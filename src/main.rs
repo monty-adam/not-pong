4c94af835a899be09baf5f321bde339d8905613b
Adam Cowan <adamcowan@adams-mbp.home>
2023-07-08 18:50:57 +0100

    ball has collision physics

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 extern crate termion;
 
-use std::{io::{self, Write}, time::{Duration, Instant}, cmp};
+use std::{
+    cmp,
+    io::{self, Write},
+    time::{Duration, Instant},
+};
 use termion::{
     event::Key,
     input::TermRead,
@@ -8,34 +12,76 @@ use termion::{
     screen::{IntoAlternateScreen, ToAlternateScreen, ToMainScreen},
 };
 
-fn draw_player_paddle<W: Write>(screen: &mut W, y_coordinate: u16) {
-    write!(
-        screen,
-        "{}{}\u{2588}{}\u{2588}{}\u{2588}",
-        termion::clear::All,
-        termion::cursor::Goto(4, y_coordinate),
-        termion::cursor::Goto(4, y_coordinate + 1),
-        termion::cursor::Goto(4, y_coordinate + 2),
-    ).unwrap();
-}
-
-fn draw_ball<W: Write>(screen: &mut W, x_coordinate: u16, y_coordinate: u16) {
-    write!(
-        screen,
-        "{}\u{2588}{}\u{2588}",
-        termion::cursor::Goto(x_coordinate, y_coordinate + 1),
-        termion::cursor::Goto(x_coordinate + 1, y_coordinate + 1),
-    ).unwrap();
-}
-
-fn draw_rival_paddle<W: Write>(screen: &mut W, x_coordinate: u16, y_coordinate: u16) {
+fn draw_paddle<W: Write>(screen: &mut W, paddle: &Paddle) {
     write!(
         screen,
         "{}\u{2588}{}\u{2588}{}\u{2588}",
-        termion::cursor::Goto(x_coordinate, y_coordinate),
-        termion::cursor::Goto(x_coordinate, y_coordinate + 1),
-        termion::cursor::Goto(x_coordinate, y_coordinate + 2),
-    ).unwrap();
+        termion::cursor::Goto(paddle.x_coordinate, paddle.y_coordinate),
+        termion::cursor::Goto(paddle.x_coordinate, paddle.y_coordinate + 1),
+        termion::cursor::Goto(paddle.x_coordinate, paddle.y_coordinate + 2),
+    )
+    .unwrap();
+}
+
+fn draw_ball<W: Write>(screen: &mut W, ball: &Ball) {
+    write!(
+        screen,
+        "{}\u{2588}{}\u{2588}",
+        termion::cursor::Goto(ball.x_coordinate, ball.y_coordinate + 1),
+        termion::cursor::Goto(ball.x_coordinate + 1, ball.y_coordinate + 1),
+    )
+    .unwrap();
+}
+
+enum BallDirection {
+    ToPlayer,
+    FromPlayer,
+}
+struct Ball {
+    x_coordinate: u16,
+    y_coordinate: u16,
+    direction: BallDirection,
+}
+
+impl Ball {
+    fn new(x_coordinate: u16, y_coordinate: u16) -> Self {
+        Self {
+            x_coordinate,
+            y_coordinate,
+            direction: BallDirection::FromPlayer,
+        }
+    }
+
+    fn check_collision(&mut self, paddle: &Paddle) {
+        if self.y_coordinate == paddle.y_coordinate {
+            if self.x_coordinate == paddle.x_coordinate + 1 {
+                self.direction = BallDirection::FromPlayer;
+            } else if self.x_coordinate == paddle.x_coordinate - 2 {
+                self.direction = BallDirection::ToPlayer;
+            };
+        };
+    }
+}
+
+struct Paddle {
+    x_coordinate: u16,
+    y_coordinate: u16,
+}
+
+impl Paddle {
+    fn new_player(y_coordinate: u16) -> Self {
+        Self {
+            x_coordinate: 4,
+            y_coordinate,
+        }
+    }
+
+    fn new_rival(x_coordinate: u16, y_coordinate: u16) -> Self {
+        Self {
+            x_coordinate,
+            y_coordinate,
+        } 
+    }
 }
 
 fn main() {
@@ -52,55 +98,50 @@ fn main() {
             .unwrap();
 
         let mut keys_pressed = stdin.keys();
-        
-        write!(
-            screen,
-            "{}{}",
-            termion::clear::All,
-            termion::cursor::Hide,
-        ).unwrap();
+
+        write!(screen, "{}{}", termion::clear::All, termion::cursor::Hide,).unwrap();
 
         // Starting variables
-        let mut player_location = 1;
-        let mut ball_location = 5;
-        let mut move_right = true;
+        // let mut player_location = 1;
+        // let mut ball_location = 5;
+        // let mut move_right = true;
+        let mut player = Paddle::new_player(1);
+        let rival = Paddle::new_rival(term_width, 1);
+        let mut ball = Ball::new(5, 1);
 
         // Init game
         let mut start = Instant::now();
+        draw_paddle(&mut screen, &player);
+        draw_paddle(&mut screen, &rival);
+        draw_ball(&mut screen, &ball);
         screen.flush().unwrap();
-        draw_player_paddle(&mut screen, player_location);
-        draw_rival_paddle(&mut screen, term_width, 1);
-        draw_ball(&mut screen, ball_location, player_location);
 
         loop {
             let duration = start.elapsed();
             if duration >= Duration::from_millis(33) {
                 start = Instant::now();
 
-                if ball_location == 5 {
-                    move_right = true;
-                } else if ball_location == term_width - 2 {
-                    move_right = false;
-                };
-    
-                ball_location = if move_right {
-                    cmp::min(ball_location + 1, term_width - 2)
-                } else {
-                    cmp::max(ball_location - 1, 5)
+                ball.x_coordinate = match ball.direction {
+                    BallDirection::FromPlayer => cmp::min(ball.x_coordinate + 1, term_width),
+                    BallDirection::ToPlayer => cmp::max(ball.x_coordinate - 1, term_width / term_width),
                 };
 
+                write!(screen, "{}", termion::clear::All).unwrap();
+                draw_paddle(&mut screen, &player);
+                draw_paddle(&mut screen, &rival);
+                draw_ball(&mut screen, &ball);
                 screen.flush().unwrap();
-                draw_player_paddle(&mut screen, player_location);
-                draw_rival_paddle(&mut screen, term_width, 1);
-                draw_ball(&mut screen, ball_location, player_location);
             }
 
+            ball.check_collision(&player);
+            ball.check_collision(&rival);
+
             if let Some(c) = keys_pressed.next() {
-                player_location = match c.unwrap() {
+                player.y_coordinate = match c.unwrap() {
                     Key::Char('q') => break,
-                    Key::Char('z') => cmp::max(player_location - 1, 1),
-                    Key::Char('x') => cmp::min(player_location + 1, term_height - 3),
-                    Key::Null | _ => player_location,
+                    Key::Char('z') => cmp::max(player.y_coordinate - 1, 1),
+                    Key::Char('x') => cmp::min(player.y_coordinate + 1, term_height - 3),
+                    Key::Null | _ => player.y_coordinate,
                 };
             };
         }
